fa2e34ca7cd43b569c93069c0cbd0aaf7039b572
UmedjonD <lazerr_94@mail.ru>
2018-12-03 18:21:28 +0400

    the end

--- src/main/java/Regexps.java
+++ src/main/java/Regexps.java
@@ -1,53 +1,48 @@
-import javafx.scene.shape.Path;
-
 import java.io.BufferedReader;
-import java.io.File;
+import java.io.BufferedWriter;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
 public class Regexps {
+
     private static Charset charset;
 
-//    private static final String inFile = ("src/main/java/file/inMessage.txt");
-//    private static final String outFile = ("src/main/java/file/outMessage.txt");
-
-    
     public static void main(String[] args) {
-        Path inFile = (Path) Paths.get("src/main/java/file/inMessage.txt");
-        Path outFile = (Path) Paths.get("src/main/java/file/outMessage.txt");
+        Path inFile =  Paths.get("src/main/java/file/inMessage");
+        Path outFile =  Paths.get("src/main/java/file/outMessage");
 
         readerWriteFile(inFile, outFile);
-
     }
-
     public static void readerWriteFile(Path inFile, Path outFile) {
         charset = Charset.forName("UTF-8");
         try (BufferedReader reader = Files.newBufferedReader(inFile, charset);
-             BufferedReader writer = Files.newBufferedReader(outFile, charset)) {
+             BufferedWriter writer = Files.newBufferedWriter(outFile, charset)) {
 
             String line = null;
             while ((line = reader.readLine()) != null)
                 for (String phone: getNumber(line))
-                    writer.write(phone + "n\")
+                    writer.write(phone + "\n");
+                writer.flush();
             }catch(Exception e){
-                e
+                e.printStackTrace();
             }
-
-        }
-
-        Pattern pattern = Pattern.compile("[1-3]");
-        Matcher matcher = pattern.matcher("rfrtkf1fhf123");
-
-        while(matcher.find()){
-            System.out.println(matcher.start() + " " + matcher.group());
-        }
-        System.out.println(" ");
-
     }
 
-}
+    public static List<String> getNumber(String line) {
+        List<String> phones = new ArrayList<>();
+
+        Pattern p = Pattern.compile("\\+\\d\\(\\d{3}\\) \\d{3}( \\d{2}){2}");
+        Matcher m = p.matcher(line);
+
+        while (m.find()) phones.add(m.group().replaceAll("[^\\d]", ""));
+        return phones;
+    }
+
+    }
